37a61ec760830b94121707adbfe352f722d96be6
fillu87gyc <wander.off65@gmail.com>
2016-12-24 18:57:39 +0900

    comp

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication3
 {
-    class Program
+    class TwoD
     {
         static void Main()
         {
+           // int t, i;
+            int[,] table = new int[3, 4];
+            for (int t = 0; t < 3; t++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    table[t, i] = (t * 4) + i + 1;
+                    Console.Write(table[t,i] + "\t");
+                }
+                Console.Write("{0}"+"",'\n');
+            }
         }
     }
 }
